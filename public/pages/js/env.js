1131448b482625d4ee220e60fe30042fb50b4c24
Francisco Tun <francisco.tun@avocadoblock.com>
2022-08-08 18:05:57 -0500

    update envs: remote api endpoint

--- public/pages/js/env.js
+++ public/pages/js/env.js
@@ -5,8 +5,10 @@ window.WWW_BASE = test ? 'https://genobank.io/test' : 'https://genobank.io';
 window.API_BASE = test ? 'https://api-test.genobank.io' : 'https://api.genobank.io';
 window.NAMESPACE = test ? 'io.genobank.test' : 'io.genobank';
 
-// window.NEWAPIBASE = test ? 'http://localhost:8081'
 
-window.NEWAPIBASE = 'http://localhost:8081'
+// window.NEWAPIBASE = 'http://localhost:8081'
+
+// ceate a validation to check if iam in local or external
+window.NEWAPIBASE = 'https://genobank.app/'
 
 window.PERMITTEE = test ? 'test_create_permitee':'create_permitee'
