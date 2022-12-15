839d921f8b78d335149152d6169e0e7ab7bec0ed
Francisco Tun <francisco.tun@avocadoblock.com>
2022-12-15 13:46:06 -0600

    Update: Change in the envs

--- public/pages/js/env.js
+++ public/pages/js/env.js
@@ -1,16 +1,19 @@
 const url = new URL(location.href);
 const test = url.pathname.substr(10, 5) == '/test';
+window.HOST = url.host
 window.ENV = test ? 'test' : 'main';
 window.WWW_BASE = test ? 'https://genobank.io/test' : 'https://genobank.io';
+window.WWW_LOCAL_BASE = "http://192.168.50.219:5502/test"
 window.API_BASE = test ? 'https://api-test.genobank.io' : 'https://api.genobank.io';
 window.NAMESPACE = test ? 'io.genobank.test' : 'io.genobank';
+
 window.BIOSAMPLE_ACTIVATION_BASE = test ? [
-  `${window.WWW_BASE}/activate`,
+  `${window.WWW_LOCAL_BASE}/activate`,
 ] : [
-  `${window.WWW_BASE}/activate`,
+  `${window.WWW_LOCAL_BASE}/activate`,
   `https://start.somosancestria.com`,
 ];
-
+console.log(window.HOST)
 window.favicon = test ? './../static/images/favicon-32x32.png':'./static/images/favicon-32x32.png';
 
 // window.NEWAPIBASE = 'http://localhost:8081'
